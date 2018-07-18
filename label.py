dff7b2565c697c3df73ca2d432f9009132cdb495
Patrik <kopkanpatrik@gmail.com>
2018-07-18 18:08:47 +0200

    -added setup =added partly done functions for drawing labels in svg

--- label.py
+++ label.py
@@ -1,2 +1,48 @@
 import svgwrite
+from svgwrite import px
+MARKS = ['A+++', 'A++', 'A+', 'A','B','C','D']
+
+def color(marks, index):
+    steps_of_color = int(256/len(marks)*2) - 1
+    index_of_green_label = 0
+    index_of_yellow_label = int(len(marks)/2)
+    index_of_red_label = len(marks)-1
+
+    if index < index_of_yellow_label:
+        r = abs(0 - index) * steps_of_color
+        g = 255
+        return r, g, 0
+
+    elif index > index_of_yellow_label:
+        g = 255 - abs(index_of_yellow_label - index) * steps_of_color
+        r = 255
+        return r, g, 0
+    return 255, 255, 0 # yellow label
+
+def generate_labels(height, width, marks):
+
+    steps_of_width = width/(2 * len(marks)) # first label has half of width
+    steps_of_height = height / ((2*len(marks) - 1))
+    height_of_labels = steps_of_height
+
+    w_str = "{}pt".format(width)
+    h_str = "{}pt".format(height)
+    dwg = svgwrite.Drawing('labels.svg', (w_str, h_str))
+    label = dwg.add(dwg.g(id='label', stroke='green'))
+    i = 1
+    points = [
+        [0, 0],
+        [width/2, 0],
+        [width/2 + 30, height_of_labels/2],
+        [width/2, height_of_labels],
+        [0, height_of_labels],
+        [0, 0]
+    ]
+
+    polygon = svgwrite.shapes.Polygon(points=points)
+    label.add(dwg.line(start=(0*px, 0*px), end=(200*px, 200*px)))
+    label.add(polygon)
+    dwg.save()
+
+generate_labels(1000,1000,MARKS)
 
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+echo "Create Virtualenv for Python deps ..."
+function prepare_venv() {
+    VIRTUALENV=$(which virtualenv)
+    if [ $? -eq 1 ]
+    then
+        # python34 which is in CentOS does not have virtualenv binary
+        VIRTUALENV=$(which virtualenv-3)
+    fi
+
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    if [ $? -ne 0 ]
+    then
+        printf "%sPython virtual environment can't be initialized%s" "${RED}" "${NORMAL}"
+        exit 1
+    fi
+    pip install -U pip
+    python3 "$(which pip3)" install -r requirements.txt
+
+}
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
